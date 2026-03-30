f9a870ef2cac6349f83547e872c999e13d4aa757
Lucas Prado <troyscripter@gmail.com>
2026-03-30 00:24:01 -0300

    Spring Boot application created

---
+++ src/main/java/com/example/spring_logistics_integrator/SpringLogisticsIntegratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_logistics_integrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogisticsIntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogisticsIntegratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_logistics_integrator/SpringLogisticsIntegratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_logistics_integrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogisticsIntegratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
